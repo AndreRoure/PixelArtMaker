b091b36fa377cfb504ff7f3cb3e18d2f8dab8159
Andre_Roure <roure.news@gmail.com>
2020-06-13 14:56:55 -0300

    Initial Commit
    The project was done before the creation of the repo.

---
+++ README.md
@@ -0,0 +1,5 @@
+A web page for Pixel Art Maker
+
+To use open index.html on a browser.
+
+This project was part of the Udacity Nanodegree Program Intro to Programming
---
+++ designs.js
@@ -0,0 +1,45 @@
+//Necessary variables.
+const grid = document.getElementById("pixelCanvas");
+const gridSize = document.getElementById("sizePicker");
+const colorPicker = document.getElementById("colorPicker");
+let color = "#000000";
+
+
+//Function that generates the grid (activated by event listener).
+function makeGrid() {
+  let height = document.getElementById("inputHeight").value;
+  let width = document.getElementById("inputWidth").value;
+  let first = "";
+  let second = "";
+//Loop to detrmine the width of the grid.
+for (let i = 1; i <= width; i++) {
+  first = first + "<td></td>";
+}
+//Loop to determine the height of the grid(needs the width).
+for (let j = 1; j <= height; j++) {
+  second = second + "<tr>" + first + "</tr>";
+}
+//Creates the grid.
+grid.innerHTML = second;
+}
+
+//Event listener for the submition of grid size.
+gridSize.addEventListener("submit", function(e){
+  //Stop the grid from reseting by itself.
+  e.preventDefault();
+  makeGrid();
+});
+
+//Event listener for color change.
+colorPicker.addEventListener("change", function(){
+  return color = colorPicker.value;
+});
+
+//Event listener for painting the selected pixel.
+grid.addEventListener("click", function (e) {
+  if (e.target.tagName == "TD") {
+    e.target.style.backgroundColor = color;
+  }
+});
+
+//by André Carvalho de Roure
